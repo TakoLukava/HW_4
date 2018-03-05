cd36c5c025e9a87e87f58e58d9a29af33492f106
TakoLukava <34337042+TakoLukava@users.noreply.github.com>
2018-03-05 23:18:34 +0300

    Update hw3_wk4.py

--- hw3_wk4.py
+++ hw3_wk4.py
@@ -1,24 +1,13 @@
-data = []
-data.append({'name': 'Immanuel', 'surname': 'Kant', 'sex': 'male', 'age': '12'})
-data.append({'name': 'Georg', 'surname': 'Hegel', 'sex': 'male', 'age': '15'})
-data.append({'name': 'Lou', 'surname': 'Andreas-Salome', 'sex': 'female', 'age': '13'})
-data.append({'name': 'Arthur', 'surname': 'Schopenhauer', 'sex': 'male', 'age': '17'})
-data.append({'name': 'Friedrich', 'surname': 'Nietzsche', 'sex': 'male', 'age': '12'})
+data = [{'name': 'Immanuel', 'surname': 'Kant', 'sex': 'male', 'age': '12'},
+{'name': 'Georg', 'surname': 'Hegel', 'sex': 'male', 'age': '15'},
+{'name': 'Lou', 'surname': 'Andreas-Salome', 'sex': 'female', 'age': '13'},
+{'name': 'Arthur', 'surname': 'Schopenhauer', 'sex': 'male', 'age': '17'},
+{'name': 'Friedrich', 'surname': 'Nietzsche', 'sex': 'male', 'age': '12'}]
 
 put = input('Enter value to search:')
+num=list(enumerate(data, start=1))
 
-def num():
-    for i in range(len(data)):
-        i+=1
-        return i
-
-def output():
-    while True:
-        for j in data:
-            if j['name'] == put or j['sex'] == put:
-                return j
-            else:
-                pass  
-
-
-print(num(), output())
+for j in data:
+    if j['name'] == put or j['sex'] == put:
+        j
+     print(j)
